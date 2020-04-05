cfacb543d71eca37434de55042de38765dc7d49a
Daryl Mendoza <darylemb@gmail.com>
2020-04-05 16:15:52 -0500

    final code

--- exercise2short.py
+++ exercise2short.py
@@ -2,23 +2,17 @@
 import Queue
 class Warehouse():   #Clase creada almacen para resolver el problema planteado
     qPackets = Queue.LifoQueue() #atributo queue tipo LiFo para guardar los paquetes
-    qMoney = Queue.LifoQueue()   #atributo queue tipo LiFo para llevar el registro de deuda
 
-    def add(self,packets,price): #método agregar o compra, en el cual se agregan a los queues los valores que se ingresarán por consola
+    def add(self,packets): #método agregar o compra, en el cual se agregan a los queues los valores que se ingresarán por consola
         for i in range(packets):
             self.qPackets.put(1)
-            for i in range(price):
-                self.qMoney.put(1)
 
-    def sub(self,packets,price):  #método sustraer o vender, en el que se eliminan de los queues los valores que se ingresan por consola
+    def sub(self,packets):  #método sustraer o vender, en el que se eliminan de los queues los valores que se ingresan por consola
         for i in range(packets):
             self.qPackets.get(1)
-            for i in range(price):
-                self.qMoney.get(1)
 
     def totalData(self): #método de información que mostrará por pantalla el contenido final de los queues
         print("The total packets is: ",self.qPackets.qsize())
-        print("the debt is:",self.qMoney.qsize(),"\n")
 
 warehouse = Warehouse() #Se crea una instancia de Clase Warehouse
 week = ["monday","tuesday","wednesday","thursday","friday","saturday"] #Array con los días de la semana para mejor presentación
@@ -30,17 +24,13 @@ for i in range(0,len(week)): #Ciclo que registra la información por día de la 
     if option == 1: #Compra de paquetes
         print("option selected: purchase")
         print("Write the number of packets")
-        packets = int(input()) 
-        print("Write the price for packet") 
-        price = int(input())
-        warehouse.add(packets,price) #Agrega a los queues del objeto mediante el método add
+        packets = int(input())
+        warehouse.add(packets) #Agrega a los queues del objeto mediante el método add
     if option == 2:
         print("option selected: sale")
         print("Write the number of packets")
         packets = int(input())
-        print("Write the price for packet") 
-        price = int(input())
-        warehouse.sub(packets,price)#Elimina de los queues del objeto mediante el método sub
+        warehouse.sub(packets)#Elimina de los queues del objeto mediante el método sub
 
 print("\nTOTAL AT THE END OF THE WEEK:\n")
 warehouse.totalData() #Se llama al método totalData para mostrar el contenido total de paquetes y deuda, resolviendo el problema
